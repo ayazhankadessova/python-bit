de116827e7c16bd15f25f9190c08e725cb10273e
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-12-27 14:25:14 +0800

    add ml ti theme toggle

--- components/ui/theme-toggle.tsx
+++ components/ui/theme-toggle.tsx
@@ -16,7 +16,7 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' size='sm' className='w-10 px-0'>
+        <Button variant='ghost' size='sm' className='w-10 px-0 ml-2.5'>
           <Sun className='h-[1.1rem] w-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <Moon className='absolute h-[1.1rem] w-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle Theme</span>
