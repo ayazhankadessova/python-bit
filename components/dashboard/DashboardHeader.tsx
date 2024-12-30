6786b31c7ba7ee71b7a63a98b16bcf496eead5ad
ayazhankadessova <kadessovaayazhan@gmail.com>
2024-12-30 15:42:03 +0800

    added authmodal to layout

--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -17,7 +17,7 @@ export function DashboardHeader({
         <h1 className='text-4xl font-bold mb-2'>{title}</h1>
         <p className='text-gray-600'>{subtitle}</p>
       </div>
-      <Button variant='outline' onClick={onSignOut}>
+      <Button variant='secondary' onClick={onSignOut}>
         Sign Out
       </Button>
     </div>
