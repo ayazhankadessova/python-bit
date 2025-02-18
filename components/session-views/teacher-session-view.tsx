a84981ad3a324716f8376defa65f542113353682
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-02-18 14:02:30 +0800

    del unused

--- components/session-views/teacher-session-view.tsx
+++ components/session-views/teacher-session-view.tsx
@@ -32,7 +32,7 @@ import {
 import { MarkdownRenderer } from '@/components/markdown-renderer'
 import { formatCode } from '@/lib/utils'
 import { PythonEditor } from '@/components/session-views/live-session-code-editor'
-import { ActiveStudent } from '@/types/classrooms/live-session'
+// import { ActiveStudent } from '@/types/classrooms/live-session'
 // import AssignmentProgress from '@/components/session-views/lesson-progress-card'
 
 interface TeacherSessionViewProps {
@@ -277,36 +277,36 @@ export function TeacherSessionView({
     return () => unsubscribe()
   }, [classroomId, sessionId, selectedStudentUsername])
 
-  const handleStudentSelect = async (
-    studentUsername: string
-  ): Promise<void> => {
-    console.log('handleStudentSelect called with:', studentUsername)
-    console.log('Current selectedStudentUsername:', selectedStudentUsername)
+  // const handleStudentSelect = async (
+  //   studentUsername: string
+  // ): Promise<void> => {
+  //   console.log('handleStudentSelect called with:', studentUsername)
+  //   console.log('Current selectedStudentUsername:', selectedStudentUsername)
 
-    // If clicking on already selected student, deselect them
-    if (selectedStudentUsername === studentUsername) {
-      console.log('Attempting to deselect student')
-      setSelectedStudentUsername(null)
-      console.log('Current assignment:', currentAssignment)
-      if (currentAssignment) {
-        console.log('Setting teacher code to starter code')
-        setTeacherCode(formatCode(currentAssignment.starterCode))
-      }
-      return
-    }
+  //   // If clicking on already selected student, deselect them
+  //   if (selectedStudentUsername === studentUsername) {
+  //     console.log('Attempting to deselect student')
+  //     setSelectedStudentUsername(null)
+  //     console.log('Current assignment:', currentAssignment)
+  //     if (currentAssignment) {
+  //       console.log('Setting teacher code to starter code')
+  //       setTeacherCode(formatCode(currentAssignment.starterCode))
+  //     }
+  //     return
+  //   }
 
-    // Otherwise, select the new student
-    setSelectedStudentUsername(studentUsername)
-    if (currentSession) {
-      const sessionDoc = await getDoc(
-        doc(fireStore, `classrooms/${classroomId}/sessions`, currentSession.id)
-      )
-      const data = sessionDoc.data() as LiveSession
-      if (data.students[studentUsername]) {
-        setStudentCode(data.students[studentUsername].code)
-      }
-    }
-  }
+  //   // Otherwise, select the new student
+  //   setSelectedStudentUsername(studentUsername)
+  //   if (currentSession) {
+  //     const sessionDoc = await getDoc(
+  //       doc(fireStore, `classrooms/${classroomId}/sessions`, currentSession.id)
+  //     )
+  //     const data = sessionDoc.data() as LiveSession
+  //     if (data.students[studentUsername]) {
+  //       setStudentCode(data.students[studentUsername].code)
+  //     }
+  //   }
+  // }
 
   const endSession = async () => {
     try {
