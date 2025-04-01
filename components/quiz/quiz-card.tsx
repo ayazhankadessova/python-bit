eafa75e4c219523f5f3518157fd7a54502ae296b
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-04-01 11:25:48 +0800

    add placeholder image for quizzes

--- components/quiz/quiz-card.tsx
+++ components/quiz/quiz-card.tsx
@@ -9,6 +9,7 @@ import {
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { BookOpen } from 'lucide-react'
+import Image from 'next/image'
 
 interface QuizCardProps {
   id: string
@@ -27,6 +28,16 @@ export default function QuizCard({
 }: QuizCardProps) {
   return (
     <Card className='w-full'>
+      {/* Image Section */}
+      <div className='relative w-full aspect-[16/9]'>
+        <Image
+          src='/quizzes/python101-1-what-is-python/cover.jpg'
+          alt={'quiz-cover'}
+          layout='fill'
+          objectFit='cover'
+          className='rounded-t-md'
+        />
+      </div>
       <CardHeader>
         <CardTitle className='flex items-center gap-2'>
           <BookOpen className='h-5 w-5' />
