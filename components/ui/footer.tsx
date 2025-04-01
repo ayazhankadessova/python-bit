c96c0a05c57df07627add9450b64e650aad1bd59
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-04-01 16:11:48 +0800

    fixed footer link

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -24,11 +24,10 @@ export function BlogFooter() {
             </Link>
             <p className='text-sm text-muted-foreground mb-4'>
               PythonBit is an interactive educational platform designed to teach
-              Python programming through engaging
-              tutorials, hands-on projects, and real-time classroom experiences.
-              Our mission is to bridge the gap between block-based and
-              text-based programming while making coding accessible, fun, and
-              relevant for young learners.
+              Python programming through engaging tutorials, hands-on projects,
+              and real-time classroom experiences. Our mission is to bridge the
+              gap between block-based and text-based programming while making
+              coding accessible, fun, and relevant for young learners.
             </p>
             <AllSocials />
           </div>
@@ -97,7 +96,17 @@ export function BlogFooter() {
         <div className='flex flex-col sm:flex-row items-center gap-2 text-sm text-muted-foreground'>
           <p>© 2025 {siteConfig.name}</p>
           <span className='hidden sm:inline'>•</span>
-          <p>Made with ♥ by ayazhankad</p>
+          <p>
+            Made with ♥ by{' '}
+            <a
+              href='https://ayazhankad.vercel.app'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-950/50 hover:text-rose-400 transition-colors underline underline-offset-2 underline-thickness-1 decoration-green-950/50'
+            >
+              ayazhankad
+            </a>
+          </p>
         </div>
       </div>
     </footer>
