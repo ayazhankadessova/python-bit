f51a399df30f9c2c6a51ff48047371c47766d63c
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-04-10 10:08:03 +0800

    remove nested

--- hooks/quizzes/useQuizProgress.ts
+++ hooks/quizzes/useQuizProgress.ts
@@ -26,7 +26,7 @@ export function useQuizProgress(
   const shouldFetch = Boolean(quizId && user)
 
   // Create the path-based URL
-  const url = shouldFetch ? `/api/progress/quiz/${user?.uid}/${quizId}` : null
+  const url = shouldFetch ? `/api/progress/quiz/${user?.uid}` : null
 
   const { data, error, mutate } = useSWR<QuizProgress>(
     url,
