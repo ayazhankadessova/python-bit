74389a611e6860e8550c7fd7fd19cb3004169484
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-04-20 16:21:48 +0800

    text-xl for large btn

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -112,7 +112,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-16  px-8 py-4 text-2xl',
+        lg: 'h-16  px-8 py-4 text-xl',
         icon: 'h-9 w-9',
       },
     },
